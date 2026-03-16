f3ff502c6ff78c214a317bab248a334473a33e3b
mattiacazzolla <mattiacazzolla@hotmail.it>
2026-03-16 16:54:37 +0100

    BIDS

--- scripts/select_reference_stack.py
+++ scripts/select_reference_stack.py
@@ -3,33 +3,33 @@ import sys
 import nibabel as nib
 import numpy as np
 
-mask_dir = sys.argv[1]
+subject = sys.argv[1]
+session = sys.argv[2]
 
-results = []
+mask_axial_run_01 = f"envau/work/meca/data/BaboFet_BIDS/derivatives/svrtk/dwi/{subject}/{session}/{subject}_{session}_dir-AP_run-01_desc-brain_mask.nii.gz"
+mask_axial_run_02 = f"envau/work/meca/data/BaboFet_BIDS/derivatives/svrtk/dwi/{subject}/{session}/{subject}_{session}_dir-AP_run-02_desc-brain_mask.nii.gz"
 
-for acq in os.listdir(mask_dir):
-    if not acq.startswith("acq-ax"):
-        continue  # only axial acquisitions
+if not os.path.exists(mask_axial_run_01):
+    print(f"{subject}_{session}_dir-AP_run-01")
+elif not os.path.exists(mask_axial_run_02):
+    print(f"{subject}_{session}_dir-AP_run-02")
+else:
+    mask_axial_run_01_data = nib.load(mask_axial_run_01).get_fdata()
+    mask_axial_run_02_data = nib.load(mask_axial_run_02).get_fdata()
 
-    acq_path = os.path.join(mask_dir, acq)
+    edge_counts_run01 = []
+    edge_counts_run02 = []
 
-    # find nifti
-    for f in os.listdir(acq_path):
-        if f.endswith(".nii.gz"):
-            mask_file = os.path.join(acq_path, f)
-            break
-
-    mask_data = nib.load(mask_file).get_fdata()
-
-    # compute edge voxels
-    edge_counts = []
     for axis in range(3):
-        edge_counts.append(np.count_nonzero(mask_data.take(indices=0, axis=axis)))
-        edge_counts.append(np.count_nonzero(mask_data.take(indices=-1, axis=axis)))
-    total_edge_voxels = sum(edge_counts)
+        edge_counts_run01.append(np.count_nonzero(mask_axial_run_01_data.take(indices=0, axis=axis)))
+        edge_counts_run01.append(np.count_nonzero(mask_axial_run_01_data.take(indices=-1, axis=axis)))
 
-    results.append((acq, total_edge_voxels))
+        edge_counts_run02.append(np.count_nonzero(mask_axial_run_02_data.take(indices=0, axis=axis)))
+        edge_counts_run02.append(np.count_nonzero(mask_axial_run_02_data.take(indices=-1, axis=axis)))
 
-# pick best (lowest edge voxels)
-best_acq = min(results, key=lambda x: x[1])
-print(best_acq[0])
+    total_edge_voxels_run01 = sum(edge_counts_run01)
+    total_edge_voxels_run02 = sum(edge_counts_run02)
+    if total_edge_voxels_run01 <= total_edge_voxels_run02:
+        print(f"{subject}_{session}_dir-AP_run-01")
+    else:
+        print(f"{subject}_{session}_dir-AP_run-02")
