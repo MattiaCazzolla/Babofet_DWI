202fe80a5433939a5841d01cf5689f6714931aa4
mattiacazzolla <mattiacazzolla@hotmail.it>
2026-03-16 16:54:37 +0100

    BIDS

--- 999_snapshots.sh
+++ 999_snapshots.sh
@@ -14,28 +14,31 @@ MASK="${PARCELLATION_DIR}/tissue_segmentation_in_dwi.nii.gz"
 
 x_dim=$(fslval "$ADC" dim1)
 y_dim=$(fslval "$ADC" dim2)
+z_dim=$(fslval "$ADC" dim3)
+
 num_slices=$(fslval "$ADC" dim3)
 
 x_center=$((x_dim / 2))
 y_center=$((y_dim / 2))
+z_center=$((z_dim / 2))
 
 
-for (( i=0; i<$num_slices; i++ )); do
-    output_png=$(printf "${SNAPSHOTS_DIR}/parcellation_%04d.png" $i)
-
-    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
-    -of "$output_png" \
-    -s ortho \
-    --voxelLoc "$x_center" "$y_center" "$i" \
-    -sz 600 900 \
-    -xh -yh \
-    -zz 785 \
-    -hc \
-    -hl \
-    "$MASK" -ot label --lut freesurfercolorlut --outlineWidth 1 --volume 0
-done
-
-
+#for (( i=0; i<$num_slices; i++ )); do
+#    output_png=$(printf "${SNAPSHOTS_DIR}/parcellation_%04d.png" $i)
+#
+#    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+#    -of "$output_png" \
+#    -s ortho \
+#    --voxelLoc "$x_center" "$y_center" "$i" \
+#    -sz 600 900 \
+#    -xh -yh \
+#    -zz 785 \
+#    -hc \
+#    -hl \
+#    "$MASK" -ot label --lut freesurfercolorlut --outlineWidth 1 --volume 0
+#done
+#
+#
 #for (( i=0; i<$num_slices; i++ )); do
 #    output_png=$(printf "${SNAPSHOTS_DIR}/T2_%04d.png" $i)
 #
@@ -50,36 +53,156 @@ done
 #    -hl \
 #    "$T2" -ot volume -cm greyscale -dr 0 1500 
 #done
-#
-#
-#for (( i=0; i<$num_slices; i++ )); do
-#    output_png=$(printf "${SNAPSHOTS_DIR}/cFA_%04d.png" $i)
-#
-#    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
-#    -of "$output_png" \
-#    -s ortho \
-#    --voxelLoc "$x_center" "$y_center" "$i" \
-#    -sz 600 900 \
-#    -xh -yh \
-#    -zz 785 \
-#    -hc \
-#    -hl \
-#    "$VEC" -ot rgbvector -b 60 -c 70
-#done
-#
-#
-#for (( i=0; i<$num_slices; i++ )); do
-#    output_png=$(printf "${SNAPSHOTS_DIR}/MD_%04d.png" $i)
-#
-#    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
-#    -of "$output_png" \
-#    -s ortho \
-#    --voxelLoc "$x_center" "$y_center" "$i" \
-#    -sz 600 900 \
-#    -xh -yh \
-#    -zz 785 \
-#    -hc \
-#    -hl \
-#    "$ADC" -ot volume -cm greyscale -dr 0 0.003
-#done
+
+
+################################# cFA ######################################
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/cFA_coronal_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$x_center" "$i" "$z_center" \
+    -sz 600 900 \
+    -xh -zh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$VEC" -ot rgbvector -b 60 -c 70
+done
+
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/cFA_sagittal_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$i" "$y_center" "$z_center" \
+    -sz 600 900 \
+    -zh -yh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$VEC" -ot rgbvector -b 60 -c 70
+done
+
+
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/cFA_axial_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$x_center" "$y_center" "$i" \
+    -sz 600 900 \
+    -xh -yh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$VEC" -ot rgbvector -b 60 -c 70
+done
+
+
+
+
+################################# FA ######################################
+
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/FA_axial_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$x_center" "$y_center" "$i" \
+    -sz 600 900 \
+    -xh -yh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$FA" -ot volume -cm greyscale -dr 0 1
+done
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/FA_coronal_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$x_center" "$i" "$z_center" \
+    -sz 600 900 \
+    -xh -zh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$FA" -ot volume -cm greyscale -dr 0 1
+done
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/FA_sagittal_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$i" "$y_center" "$z_center" \
+    -sz 600 900 \
+    -zh -yh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$FA" -ot volume -cm greyscale -dr 0 1
+done
+
+
+################################# MD ######################################
+
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/MD_axial_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$x_center" "$y_center" "$i" \
+    -sz 600 900 \
+    -xh -yh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$ADC" -ot volume -cm greyscale -dr 0 0.003
+done
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/MD_coronal_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$x_center" "$i" "$z_center" \
+    -sz 600 900 \
+    -xh -zh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$ADC" -ot volume -cm greyscale -dr 0 0.003
+done
+
+
+for (( i=0; i<$num_slices; i++ )); do
+    output_png=$(printf "${SNAPSHOTS_DIR}/MD_sagittal_%04d.png" $i)
+
+    PYTHONWARNINGS=ignore MPLBACKEND=Agg render \
+    -of "$output_png" \
+    -s ortho \
+    --voxelLoc "$i" "$y_center" "$z_center" \
+    -sz 600 900 \
+    -zh -yh \
+    -zz 785 \
+    -hc \
+    -hl \
+    "$ADC" -ot volume -cm greyscale -dr 0 0.003
+done
 
---
+++ config/config.sh
@@ -0,0 +1,29 @@
+# --------- Directories Parameters ---------
+BABOFET_BIDS_DIR=/envau/work/meca/data/BaboFet_BIDS
+RAWDATA_DIR=${BABOFET_BIDS_DIR}/sourcedata/raw
+DERIVATIVES_DIR=${BABOFET_BIDS_DIR}/derivatives
+OUTPUT_TMP_DIR=${DERIVATIVES_DIR}/intermediate/svrtk
+
+
+# --------- Reconstruction ---------
+SVR_B0_RESOLUTION="0.5"      # Isotropic resolution for b0 SVR
+SVR_B0_ITERATIONS="6"        # Iterations for b0 SVR
+
+SVR_B1000_RESOLUTION="0.5"      # Isotropic resolution for b0 SVR
+SVR_B1000_ITERATIONS="6"        # Iterations for b0 SVR
+
+SVR_DWI_RESOLUTION="0.5"     # Isotropic resolution for the final DWI reconstruction
+SVR_DWI_ITERATIONS="10"      # Iterations for dwi SVR
+SVR_DWI_SH_ORDER="4"         # Spherical Harmonics order
+SVR_DWI_BVAL="1000"          # The b-value shell to reconstruct
+
+
+# --------- SOFTWARE ---------
+mirtk_path=/envau/work/meca/users/cazzolla.m/baboons/Babofet_DWI/sif_images/mirtk.sif
+svrtk_path=/envau/work/meca/users/cazzolla.m/baboons/Babofet_DWI/sif_images/svrtk.sif
+
+C3D_TOOL_PATH=/envau/work/meca/users/cazzolla.m/baboons/Babofet_DWI/tools/c3d_affine_tool
+
+# --------- Other Parameters ---------
+NTHR=64
+
